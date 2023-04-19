56804cb2150d93ef4f58837ca6fd2bd07008d866
Christian Tusborg <gitlab.com@terbium.dk>
2023-04-19 11:59:44 +0200

    Build

--- src/Spoon.NuGet.Core/Presentation/ClaimsPrincipalExtensions.cs
+++ src/Spoon.NuGet.Core/Presentation/ClaimsPrincipalExtensions.cs
@@ -17,4 +17,27 @@ public static class ClaimsPrincipalExtensions
         var claim = principal?.FindFirst(ClaimTypes.NameIdentifier);
         return !Guid.TryParse(claim?.Value, out var userId) ? Guid.Empty : userId;
     }
+    
+    /// <summary>
+    /// Get EmailId from ClaimsPrincipal->ClaimTypes.Email
+    /// </summary>
+    /// <param name="principal"></param>
+    /// <returns></returns>
+    public static Guid GetEmailId(this ClaimsPrincipal? principal)
+    {
+        var claim = principal?.FindFirst(ClaimTypes.Email);
+        return !Guid.TryParse(claim?.Value, out var emailId) ? Guid.Empty : emailId;
+    }    
+    
+    /// <summary>
+    /// Get Iat from ClaimsPrincipal->ClaimTypes.Iat
+    /// </summary>
+    /// <param name="principal"></param>
+    /// <returns></returns>
+    public static long GetIat(this ClaimsPrincipal? principal)
+    {
+        var claim = principal?.FindFirst("iat");
+        return !long.TryParse(claim?.Value, out var iat) ? 0 : iat;
+    }    
+    
 }
