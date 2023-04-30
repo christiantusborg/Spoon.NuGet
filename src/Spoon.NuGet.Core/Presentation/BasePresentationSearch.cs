c2b6160beb418f2893e32fd0407c2672bf2aa3bd
Christian Tusborg <gitlab.com@terbium.dk>
2023-05-01 00:53:18 +0200

    Used code clean up on BasePresentationSearch

--- src/Spoon.NuGet.Core/Presentation/BasePresentationSearch.cs
+++ src/Spoon.NuGet.Core/Presentation/BasePresentationSearch.cs
@@ -1,32 +1,35 @@
 ﻿#pragma warning disable CS8618
 namespace Spoon.NuGet.Core.Presentation;
 
-using Spoon.NuGet.Core.Domain;
+using Domain;
 
 /// <summary>
-/// Class MediatorBaseSearch. This class cannot be inherited.
+///     Class MediatorBaseSearch. This class cannot be inherited.
 /// </summary>
 public class BasePresentationSearch
 {
-  
     /// <summary>
-    ///   Gets or sets the filters.
+    ///     Gets or sets the filters.
     /// </summary>
     public List<Filter> Filters { get; init; }
+
     /// <summary>
-    ///  Gets or sets the sort field.
+    ///     Gets or sets the sort field.
     /// </summary>
     public List<Sorting> SortField { get; init; }
+
     /// <summary>
-    ///  Gets or sets the Include deleted.
+    ///     Gets or sets the Include deleted.
     /// </summary>
     public bool IncludeDeleted { get; init; }
+
     /// <summary>
-    ///  Gets or sets the page.
+    ///     Gets or sets the page.
     /// </summary>
-    public int Page {private get; init; }
+    public int Page { private get; init; }
+
     /// <summary>
-    ///  Gets or sets the page size.
+    ///     Gets or sets the page size.
     /// </summary>
-    public int PageSize {private get; init; }
+    public int PageSize { private get; init; }
 }
