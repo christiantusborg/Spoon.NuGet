343d5550fb23638e80a3451bddade8c453782874
Christian Tusborg <gitlab.com@terbium.dk>
2023-05-07 16:05:51 +0200

    Added a BaseEndpoint interface for making your own Summary and Description

---
+++ src/Spoon.NuGet.Core/Presentation/IEndpointDescription.cs
@@ -0,0 +1,14 @@
+﻿namespace Spoon.NuGet.Core.Presentation;
+
+/// <summary>
+///     Gets the description.
+/// </summary>
+/// <returns></returns>
+public interface IEndpointDescription
+{
+    /// <summary>
+    ///     Gets the description.
+    /// </summary>
+    /// <returns></returns>
+    string GetEndpointDescription();
+}
