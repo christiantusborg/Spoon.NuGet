f8d38ca3ed18179e2f3472f084e1dfc2b44e9194
Christian Tusborg <gitlab.com@terbium.dk>
2023-05-07 16:06:33 +0200

    Added a BaseEndpoint automatic Example data

---
+++ src/Spoon.NuGet.Core/Presentation/EndpointExample.cs
@@ -0,0 +1,27 @@
+﻿namespace Spoon.NuGet.Core.Presentation;
+
+/// <summary>
+///  Basic example of end examples.
+/// </summary>
+public static class EndpointExample
+{
+    /// <summary>
+    ///  Gets the example Guid.
+    /// </summary>
+    public static Guid ExampleGuid { get; } = Guid.Parse("0a0b0c0d-1e2f-3a4b-5c6d-0a0b0c0d0e0f");
+    
+    /// <summary>
+    ///  Gets the example Firstname.
+    /// </summary>
+    /// <param name="isFemale"></param>
+    /// <returns></returns>
+    public static string GetFirstname(bool isFemale = false)
+    {
+        return isFemale ? "Jane" : "John";
+    }
+    
+    /// <summary>
+    ///  Gets the example Lastname.
+    /// </summary>
+    public static string ExampleLastname { get; } = "Doe";
+}
