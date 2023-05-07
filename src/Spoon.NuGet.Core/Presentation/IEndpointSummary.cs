65ce11ab2a9f155ab1b6c7a1283c58aa5673dd0d
Christian Tusborg <gitlab.com@terbium.dk>
2023-05-07 16:06:02 +0200

    Added a BaseEndpoint interface for making your own Summary and Description

---
+++ src/Spoon.NuGet.Core/Presentation/IEndpointSummary.cs
@@ -0,0 +1,13 @@
+﻿namespace Spoon.NuGet.Core.Presentation;
+
+/// <summary>
+///  Represents the summary to create a category.
+/// </summary>
+public interface IEndpointSummary
+{
+    /// <summary>
+    ///  Gets the summary.
+    /// </summary>
+    /// <returns></returns>
+    string GetEndpointSummary();
+}
