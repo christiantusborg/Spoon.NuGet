9a482ea14243f205117a23885d48c34fc92c00a2
Christian Tusborg <gitlab.com@terbium.dk>
2023-05-12 00:02:51 +0200

    Cleaned SearchRequest and BaseRequestWithSearchAndPagination

--- src/Spoon.NuGet.Core/Presentation/BaseRequestWithSearchAndPagination.cs
+++ src/Spoon.NuGet.Core/Presentation/BaseRequestWithSearchAndPagination.cs
@@ -5,105 +5,50 @@ using Domain;
 using Microsoft.AspNetCore.Http;
 
 /// <summary>
-/// Class BaseRequestWithSearchAndPagination.
+///     Class BaseRequestWithSearchAndPagination.
 /// </summary>
-public abstract partial class BaseRequestWithSearchAndPagination
+public abstract class BaseRequestWithSearchAndPagination
 {
-    private readonly List<Filter> _filters = new();
-
     /// <summary>
+    ///     Initializes a new instance of the <see cref="BaseRequestWithSearchAndPagination" /> class.
     /// </summary>
-    public List<Filter> Filters
-    {
-        get => this._filters;
-        init => this._filters = value;
-    }
-
-    ///// <summary>
-    ///// Gets the skip.
-    ///// </summary>
-    ///// <value>The skip.</value>
-    ////[FromQuery]
-    //public int Skip => (this.Page - 1) * this.PageLength;
-
-    ///// <summary>
-    ///// Gets the take.
-    ///// </summary>
-    ///// <value>The take.</value>
-    ////[FromQuery]
-    //public int Take => this.PageLength;
+    public List<Filter>? Filters { get; init; }
 
     /// <summary>
-    /// Gets the page.
+    ///     Gets or sets the page.
     /// </summary>
-    /// <value>The page.</value>
-    public int Page { get; set; }
+    public List<Sorting>? SortField { get; init; }
 
     /// <summary>
-    /// Gets the length of the page.
+    ///     Gets or sets the Include deleted.
     /// </summary>
-    /// <value>The length of the page.</value>
-    public int PageLength { get; set; }
+    public bool IncludeDeleted { get; init; }
 
     /// <summary>
+    ///     Gets or sets the page.
     /// </summary>
-    /// <param name="filter"></param>
-    public void AddFilter(Filter filter)
-    {
-        this._filters.Add(filter);
-    }
+    public int Page { get; init; }
 
     /// <summary>
-    /// Sets the page.
+    ///     Gets or sets the length of the page.
     /// </summary>
-    /// <param name="page">The page.</param>
-    public void SetPage(int page)
-    {
-        this.Page = page;
-    }
+    public int PageLength { get; init; }
+
 
     /// <summary>
-    /// Sets the length of the page.
+    ///     Sets the page.
     /// </summary>
-    /// <param name="pageLength">Length of the page.</param>
-    public void SetPageLength(int pageLength)
-    {
-        this.PageLength = pageLength;
-    }
-//}
-//public abstract partial class ApiBaseQueryWithSearchAndPagination
-//{
-    //}
-    ////namespace Getinge.NuGet.Core.Application.Mediator
-    ////{
-    ////    public class BaseRequestPaging
-    ////    {
-    ////        public int Page { get; set; } = 1;
-    ////        public int PageLength { get; set; } = 50;
-    ////    }
-    ////    public class BaseRequestSearchAndPaging : BaseRequestPaging
-    ////    {
-    ////        public bool IncludeDeleteSoftd { get; set; }
-    ////    }
-    ////}
-
-    //namespace Getinge.WebApiHelper.SharedInfo
-    //{
-    //using Microsoft.AspNetCore.Http;
-    //using System.Reflection;
-    //using System.Threading.Tasks;
     /// <summary>
-    /// 
     /// </summary>
     /// <typeparam name="TSelf"></typeparam>
     public interface IExtensionBinder<TSelf> where TSelf : IExtensionBinder<TSelf>
     {
         /// <summary>
-        /// The method discovered by RequestDelegateFactory on types used as parameters of route
-        /// handler delegates to support custom binding.
+        ///     The method discovered by RequestDelegateFactory on types used as parameters of route
+        ///     handler delegates to support custom binding.
         /// </summary>
-        /// <param name="context">The <see cref="HttpContext"/>.</param>
-        /// <param name="parameter">The <see cref="ParameterInfo"/> for the parameter being bound to.</param>
+        /// <param name="context">The <see cref="HttpContext" />.</param>
+        /// <param name="parameter">The <see cref="ParameterInfo" /> for the parameter being bound to.</param>
         /// <returns>The value to assign to the parameter.</returns>
         static abstract ValueTask<TSelf?> BindAsync(HttpContext context, ParameterInfo parameter);
     }
--- src/Spoon.NuGet.Core/Presentation/SearchRequest.cs
+++ src/Spoon.NuGet.Core/Presentation/SearchRequest.cs
@@ -47,93 +47,20 @@ public class SearchRequest : BaseRequestWithSearchAndPagination
     /// <param name="context"></param>
     /// <param name="parameter"></param>
     /// <returns></returns>
-    public static ValueTask<SearchRequest?> BindAsync(HttpContext context,
-                                               ParameterInfo parameter)
+    public static ValueTask<SearchRequest?> BindAsync(HttpContext context, ParameterInfo parameter)
     {
-        //const string sortByKey = "sortBy";
-        //const string sortDirectionKey = "sortDir";
-        //const string currentPageKey = "page";
-
-        //Enum.TryParse<SortDirection>(context.Request.Query[sortDirectionKey],
-        //                             ignoreCase: true, out var sortDirection);
-        //int.TryParse(context.Request.Query[currentPageKey], out var page);
-        //page = page == 0 ? 1 : page;
-
-        //var result = new PagingData
-        //{
-        //    SortBy = context.Request.Query[sortByKey],
-        //    SortDirection = sortDirection,
-        //    CurrentPage = page
-        //};
         var objResult = new SearchRequest()
         {
             IncludeDeletedSoft = false,
             Page = 1,
-            PageLength = 100
+            PageLength = 100,
+            IncludeDeleted = false,
+            SortField = null,
         };
-        //}
         objResult = GetFromQueryString<SearchRequest>(context.Request.QueryString.Value, objResult);
         return ValueTask.FromResult<SearchRequest?>(objResult);
     }
 
-    //    public static ValueTask<SearchRequest?> BindAsync(HttpContext context, ParameterInfo parameter)
-    //    {
-    //        SearchRequest empObj = GetFromQueryString<SearchRequest>(context.Request.QueryString.Value);
-    //        return ValueTask.FromResult<SearchRequest?>(empObj);
-
-    //        //    string author = context.Request.Query["Author"];
-    //        //    int.TryParse(context.Request.Query["Page"], out var page);
-    //        //    int.TryParse(context.Request.Query["PageLength"], out var pageLength);
-    //        //    int.TryParse(context.Request.Query.["PageLength"], out var pageLength);
-    //        //     public List<Filter> Filters
-    //        //{
-    //        //    get => this._filters;
-    //        //    init => this._filters = value;
-    //        //}
-
-
-    //        //bool.TryParse(context.Request.Query["IncludeDeleteSoftd"], out var includeDeleteSoftd);
-    //        //    var result = new SearchRequest
-    //        //    {
-    //        //        Page = page,
-    //        //        PageLength= pageLength,
-    //        //        IncludeDeleteSoftd = includeDeleteSoftd
-    //        //    };
-    //        //    return ValueTask.FromResult<SearchRequest?>(result);
-    //    }
-    //    /*
-    //     public T GetFromQueryString<T>() where T : new(){
-    //    var obj = new T();
-    //    var properties = typeof(T).GetProperties();
-    //    foreach(var property in properties){
-    //        var valueAsString = HttpContext.Current.Request.QueryString[property.PropertyName];
-    //        var value = Parse( valueAsString, property.PropertyType);
-
-    //        if(value == null)
-    //            continue;
-
-    //        property.SetValue(obj, value, null);
-    //    }
-    //    return obj;
-    // }
-    //    public T GetFromQueryString<T>() where T : new()  
-    //{  
-    //   var obj = new T();  
-    //   var properties = typeof(T).GetProperties(); // to get all properties from Class(Object)  
-    //   foreach (var property in properties)  
-    //   {  
-    //      var valueAsString = HttpContext.Current.Request.QueryString[property.Name];   
-    //      object value = Parse(property.PropertyType, valueAsString); // parse data types  
-
-    //      if (value == null)  
-    //      continue;  
-
-    //      property.SetValue(obj, value, null); //set values to properties.  
-    //   }  
-    //   return obj;  
-    //}   
-
-    //     */
     /// <summary>
     /// Used for parsing as query string.
     /// </summary>
@@ -163,25 +90,9 @@ public class SearchRequest : BaseRequestWithSearchAndPagination
         }
         return obj;
     }
-    //    //public static T GetFromQueryString<T>(HttpContext context) where T : new()
-    //    //{
-    //    //    var obj = new T();
-    //    //    var properties = typeof(T).GetProperties(); // to get all properties from Class(Object)  
-    //    //    foreach (var property in properties)
-    //    //    {
-    //    //        var valueAsString = context.Request.Query[property.Name];
-    //    //        object value = Parse(property.PropertyType, valueAsString); // parse data types  
-
-    //    //        if (value == null)
-    //    //            continue;
-
-    //    //        property.SetValue(obj, value, null); //set values to properties.  
-    //    //    }
-    //    //    return obj;
-    //    //}
-    //    // all in one parse method.   
+    
     /// <summary>
-    /// 
+    /// Used for parsing as query string. 
     /// </summary>
     /// <param name="dataType"></param>
     /// <param name="valueToConvert"></param>
