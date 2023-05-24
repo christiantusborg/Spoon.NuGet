69496cb861b216b0206256147e828d1332077cbe
Christian Tusborg <christian+github.com@tusborg.dk>
2023-05-24 11:39:52 +0200

    ToCreatedResult Added

--- src/Spoon.NuGet.Core/EitherCore/Extensions/EitherResultExtensions.cs
+++ src/Spoon.NuGet.Core/EitherCore/Extensions/EitherResultExtensions.cs
@@ -60,4 +60,39 @@ public static class EitherResultExtensions
 
         return Results.NoContent();
     }
+    
+    /// <summary>
+    ///   Converts the specified <see cref="Either{TResponse}" /> instance to an <see cref="IResult" /> instance.
+    /// </summary>
+    /// <param name="either"></param>
+    /// <param name="type"></param>
+    /// <param name="noContent"></param>
+    /// <typeparam name="TResponse"></typeparam>
+    /// <returns></returns>
+    /// <exception cref="ArgumentNullException"></exception>
+    public static IResult ToCreatedResult<TResponse>(this Either<TResponse> either, Type type, bool noContent = false)
+    {
+        if (either.EitherEnum != EitherEnum.Success)
+            return either.GetFaulted().ToResult();
+
+        if (noContent)
+            return Results.NoContent();
+
+        if (either.success is null)
+            throw new ArgumentNullException("Getinge.NuGet.Core.EitherCore.Exceptions.ExceptionBase" + nameof(either.success));
+
+        var properties = typeof(TResponse).GetProperties();
+
+        if (properties.Length > 0)
+        {
+            var responseTyped = Activator.CreateInstance(type);
+            var response = either.success.Adapt(responseTyped);
+
+            return Results.Ok(response);
+        }
+
+        var responseObject = Activator.CreateInstance(type);
+
+        return Results.Json(responseObject,null,null,StatusCodes.Status201Created);
+    }
 }
